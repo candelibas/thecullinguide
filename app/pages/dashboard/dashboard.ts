95ecea1986abd17a2b177b024e8653212ccaf835
Can Delibas <candelibas96@gmail.com>
2016-03-26 22:13:30 +0200

    news, airdrops and map components added to navigation

--- app/pages/dashboard/dashboard.ts
+++ app/pages/dashboard/dashboard.ts
@@ -1,26 +1,30 @@
 import {Page, NavController, IonicApp} from 'ionic-angular';
 import {News} from '../news/news';
+import {Airdrops} from '../airdrops/airdrops';
+import {Map} from '../map/map';
+
 // https://angular.io/docs/ts/latest/api/core/Type-interface.html
 import {Type} from 'angular2/core';
+
 
 @Page({
   templateUrl: 'build/pages/dashboard/dashboard.html',
 })
 
 
-
 export class Dashboard {
 
   public pages: Array<{ title: string, component: Type }>
-
 
   constructor(private nav: NavController, private app: IonicApp) {
 
     this.nav = nav;
 
+    // Let's define the pages
     this.pages = [
       { title: 'News', component: News },
-      //{ title: 'Airdrops', component: Airdrops }
+      { title: 'Airdrops', component: Airdrops },
+      { title: 'MAP', component: Map}
 
     ];
   }
