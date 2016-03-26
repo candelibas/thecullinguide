29d9baa9ea88b85d823e5e3872cadc290fb43d87
Can Delibas <candelibas96@gmail.com>
2016-03-26 22:15:08 +0200

    news component added and fixed

--- app/pages/news/news.ts
+++ app/pages/news/news.ts
@@ -1,11 +1,28 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page, NavController, NavParams} from 'ionic-angular';
+import {NewsService} from '../../providers/news.service';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {NewsDetail} from './newsdetail';
 
 @Page({
   templateUrl: 'build/pages/news/news.html',
+  providers: [NewsService, HTTP_PROVIDERS]
 })
 export class News {
 
-  constructor(private nav: NavController) {
+  private news;
+
+  constructor(private nav: NavController, private navParams: NavParams, private newsService: NewsService) {
     this.nav = nav;
+
+    this.navParams = navParams;
+
+    this.news = newsService.get().subscribe(
+      news => this.news = news
+    );
   }
+
+  newsDetail(title, contents) {
+    this.nav.push(NewsDetail, {title: title, contents: contents});
+  }
+
 }
