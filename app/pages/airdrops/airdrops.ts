9a8ccb399341432e51a993c38eb8e9ec1ae916df
Can Delibas <candelibas96@gmail.com>
2016-03-29 23:39:27 +0300

    feat: add ngModel for searchbar

--- app/pages/airdrops/airdrops.ts
+++ app/pages/airdrops/airdrops.ts
@@ -9,13 +9,48 @@ import {AirdropsDetail} from './airdropsdetail';
 export class Airdrops {
 
   private airdrops;
+  private searchQuery;
 
   constructor(private nav: NavController, private navParams: NavParams, private airdropService: AirdropsService) {
     this.nav = nav;
     this.navParams = navParams;
+    this.airdropService = airdropService;
 
-    this.airdrops = airdropService.get();
+    // initialize the search query
+    this.searchQuery = '';
 
+    // initialize our json data
+    this.initializeItems();
+    
   }
+
+  // Let's get our json data
+  initializeItems() {
+    this.airdrops = this.airdropService.get();
+  }
+
+  // This will be triggered by (input)
+  getItems(searchbar) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // Set q to the value of the searchbar
+    var q = searchbar.value;
+
+    // If the value is an empty string don't filter the items
+    if (q.trim() == '') {
+      return;
+    }
+
+    this.airdrops = this.airdrops.filter((v) => {
+      if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    })
+  }
+
+
+
 
 }
